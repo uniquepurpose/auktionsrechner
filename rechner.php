67402d24641fad5b363898c3283aae0c578f26d9
Christoph Holas <uniquepurpose@users.noreply.github.com>
2019-07-30 11:52:35 +0200

    Create rechner.php

---
+++ rechner.php
@@ -0,0 +1,54 @@
+<?php
+
+$i2 = 15;
+$i3 = 20;
+
+if(!empty($_POST['i1']) &&!empty($_POST['i2']) && !empty($_POST['i3'])) {
+  $i1 = $_POST['i1'];
+  $i2 = $_POST['i2'];
+  $i3 = $_POST['i3'];
+  
+  $out1 = $i1 * $i2 / 100;
+  $out2 = $i1 * $i3 / 100;
+  $out3 = $out1 + $out2;
+  
+  $out4 = $i1 + $out3;
+}
+
+?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Formel</title>
+  <style type="text/css">
+  	* { font-family: sans-serif; }
+  	input { text-align: right; }
+  </style>
+</head>
+<body>
+  <form method="post">
+    <table>
+      <tr>
+       <td>Rufpreis</td>
+       <td><input type="text" name="i1" value="<?php echo htmlentities($i1); ?>"></td>
+       <td><input type="submit" value="berechnen"></td>
+      </tr>
+      <tr>
+       <td>Auktionsgebühr</td>
+       <td><input type="text" name="i2" value="<?php echo htmlentities($i2); ?>"></td>
+       <td>€ <?php echo number_format($out1, 2, ',', '.'); ?></td>
+      </tr>
+      <tr>
+       <td>Mehrwertsteuer</td>
+       <td><input type="text" name="i3" value="<?php echo htmlentities($i3); ?>"></td>
+       <td>€ <?php echo number_format($out2, 2, ',', '.'); ?></td>
+      </tr>
+      <tr>
+       <td>Gesamt</td>
+       <td>€ <?php echo number_format($out4, 2, ',', '.'); ?></td>
+       <td>€ <?php echo number_format($out3, 2, ',', '.'); ?></td>
+      </tr>
+    </table>
+  </form>
+</body>
+</html>
